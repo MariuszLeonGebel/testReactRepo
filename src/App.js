598d00cfc6a657d235e449b2f704a9a425a55dcf
MariuszLeonGebel <mariusz.gebel.programista@gmail.com>
2022-06-12 17:49:22 +0200

    commit 6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacted
+          Learn Reacted 123456789
         </a>
       </header>
     </div>
